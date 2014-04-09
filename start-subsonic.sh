b08e79b92722052e289b9195ac1f20efab7f100f
Michael Schuerig <michael@schuerig.de>
2014-04-10 00:50:11 +0200

    Support mounting a volume at /var/subsonic.
    Requires some symlink twiddling in mounted volume.
    See Dockerfile.

--- start-subsonic.sh
+++ start-subsonic.sh
@@ -1,10 +1,12 @@
 #! /bin/sh -e
 
 music="/data/music"
+data="/var/local/subsonic"
 port=4040
 
 docker run \
   --detach \
   --publish ${port}:4040 \
   --volume "${music}:/var/music:ro" \
+  --volume "${data}:/var/subsonic" \
   mschuerig/debian-subsonic
